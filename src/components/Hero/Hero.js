66791a6a0ade1f6adee4995d04a75f45d7f5a78e
Kyrylo Tsybulskyi <k.tsybulskyi@gmail.com>
2021-02-27 20:56:33 +0100

    Aded components List and Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+const Hero = props => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src="https://i.postimg.cc/MpVBYK1P/space.png" alt="cosmos"/>
+  </header>
+);
+
+export default Hero;

2b71e6ded679e0bd72c5eb904bda947e3594f256
Kyrylo Tsybulskyi <k.tsybulskyi@gmail.com>
2021-02-27 22:34:46 +0100

    Corrected component App, List and Heros(added props for the image). Haven't work yet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List.js'
+import List from '../List/List.js';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    subtitle: PropTypes.node,
+    children: PropTypes.node,
+    image: PropTypes.text,
+  }
+  static defaultProps = {
+    title: 'My first React app',
+    image: <img className={styles.image} src="https://i.postimg.cc/MpVBYK1P/space.png" alt="cosmos" />,
+  }
   render() {
     return (
       <div id="app">
         <main className={styles.component}>
-          <h1 className={styles.title}>My first React app</h1>
+          <h1 className={styles.title}>{this.props.title}</h1>
           <h2 className={styles.subtitle}>Hello world!</h2>
-          <List />
+          <List title={['Things to do', <sup key='1'>soon!</sup>]}>
+            <div className={styles.title}>
+              {this.props.children}
+              {this.props.image}
+            </div>
+          </List>
         </main>
       </div>
     )
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from "../Hero/Hero.js"
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} />
+        <Hero titleText={this.props.title} imageText={this.props.image} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.image}>
+          {/*this.props.image*/}
+        </div>
+
       </section>
 
     )

38e9e38b07e962090407baf159c9d8d3558479a7
Kyrylo Tsybulskyi <k.tsybulskyi@gmail.com>
2021-03-04 22:25:04 +0100

    Added store from the archive

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const test = "1";
 import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App.js';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));

ce12e0a5850b9246c31d89be2a55f4c294969c0a
Kyrylo Tsybulskyi <k.tsybulskyi@gmail.com>
2021-03-21 18:22:23 +0100

    Added components Header and MainLayouts

---
+++ src/components/Home/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './Home.scss';
+import List from '../List/ListContainer.js';
+import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer.js';
+//import { pageContents, listData } from '../../data/dataStore';
+
+class Home extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.array,
+    //children: PropTypes.node,
+    //image: PropTypes.string,
+  }
+  /*static defaultProps = {
+    title: 'My first React app',
+    image: 'https://i.postimg.cc/MpVBYK1P/space.png',
+  }*/
+  render() {
+    const { title, subtitle, lists } = this.props;
+    return (
+      <main className={styles.component}>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
+      </main>
+
+    );
+  }
+}
+
+export default Home;
